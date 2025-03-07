7a1f7d90789ea53b11c8133d72724d85bd223457
Ethan060504 <burayagaxl@gmail.com>
2025-03-07 13:25:03 +0800

    Home and Restaurant Page Requests

--- model/model.js
+++ model/model.js
@@ -3,24 +3,45 @@ const Restaurant = require("./Restaurant");
 const Review = require("./Review");
 const Comment = require("./Comment");
 
+// Home Page Request
+const getTopNumRestaurants = async (num) => {
+    return await Restaurant.find({}, {_id: 1, name: 1, type: 1, rating: 1, picture_address: 1})
+                            .sort({rating: -1})
+                            .limit(num)
+                            .lean();
+}
+
+// Restaurant Page Request 
+const getRestaurantOfType = async (_type) => {
+    return await Restaurant.find({type: _type}, {_id: 1, name: 1, rating: 1, picture_address: 1})
+                            .sort({rating: -1})
+                            .lean();
+}
+
+// Getters
 const getAllUsers = async () => {
-    return await User.find({}).lean();
+    return await User.find({})
+                        .lean();
 }
 
 const getAllRestaurants = async () => {
-    return await Restaurant.find({}).lean();
+    return await Restaurant.find({})
+                            .lean();
 }
 
 const getAllReviews = async () => {
-    return await Review.find({}).lean();
+    return await Review.find({})
+                        .lean();
 }
 
 const getAllComments = async () => {
-    return await Comment.find({}).lean();
+    return await Comment.find({})
+                        .lean();
 }
 
 const getAllRestaurantsOfUser = async (userID) => {
-    return await Restaurant.find({user_id: userID}).lean();
+    return await Restaurant.find({user_id: userID})
+                            .lean();
 }
 
 module.exports = {
@@ -29,4 +50,6 @@ module.exports = {
     getAllReviews,
     getAllComments,
     getAllRestaurantsOfUser,
+    getTopNumRestaurants,
+    getRestaurantOfType,
 };
--- model/modelTest.js
+++ model/modelTest.js
@@ -8,8 +8,9 @@ const test = async () => {
     // console.log("Restaurants:", await model.getAllRestaurants());
     // console.log("Reviews:", await model.getAllReviews());
     // console.log("Comments:", await model.getAllComments());
-
-    console.log("Restaurant of User 1: ", await model.getAllRestaurantsOfUser("67c9a5edd220f3bd703c75a1"));
+    // console.log("Restaurant of User 1: ", await model.getAllRestaurantsOfUser("67c9a5edd220f3bd703c75a1"));
+    // console.log("Top 3 Restaurants: ", await model.getTopNumRestaurants(3));
+    // console.log("Italian Restaurant: ", await model.getRestaurantOfType("Italian"));
 
     mongoose.connection.close();
     process.exit();
